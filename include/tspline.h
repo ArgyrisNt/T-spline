90466c0a5851613831f64b97141a1c023c8e48da
Argiris Ntoumanis <argirisntoumanis17@hotmail.com>
2022-11-21 08:36:47 +0200

    first commit

---
+++ include/tspline.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "C:\Users\argir\OneDrive\Desktop\kVertex.h"
+
+class tspline
+{
+public:
+    // Constructors
+    tspline();
+    tspline(int, int);
+
+    // Destructor
+    ~tspline() {}
+
+    // Member functions
+    void updateKnotVecs();
+    std::vector<double> evaluate(double, double);
+
+    kVertex* kVertices;
+    int deg;
+
+private:
+    int len;
+};
+
+// i.e.
+// std::vector<double> temp{0.0,0.0,0.0,1.0,2.0,3.0,3.0,3.0};
+// double N0 = basisRecur(2.0, temp, 2, 0);
+// double N1 = basisRecur(2.0, temp, 2, 1);
+// double N2 = basisRecur(2.0, temp, 2, 2);
+// double N3 = basisRecur(2.0, temp, 2, 3);
+// double N4 = basisRecur(2.0, temp, 2, 4);
+// std::cout << N0 << "\n" << N1 << "\n" << N2 << "\n" << N3 << "\n" << N4 << std::endl;
+double basisRecur(double x, std::vector<double> knots, int deg, int knotspan)
+{
+    double N;
+    if (deg == 0)
+    {
+        if ((x >= knots[knotspan]) && (x < knots[knotspan + 1]))
+        {
+            N = 1.0;
+        }
+        else
+        {
+            N = 0.0;
+        }
+    }
+    else
+    {
+        double a = 0.0;
+        double b = 0.0;
+
+        double ad = knots[knotspan + deg] - knots[knotspan];
+        if (ad > 1e-7)
+        {
+            a = ((x - knots[knotspan]) / ad) * basisRecur(x,knots,deg-1,knotspan);
+        }
+
+        double bd = knots[knotspan + deg + 1] - knots[knotspan + 1];
+        if (bd > 1e-7)
+        {
+            b = ((knots[knotspan+deg+1] - x) / bd) * basisRecur(x, knots, deg-1, knotspan + 1);
+        }
+
+        N = a + b;
+    }
+
+    return N;
+} 
+
+double basisFunTSpl(double x, std::vector<double> knots, int deg, int knotspan)
+{
+    double N;
+    bool eq;
+    if (x == knots[0])
+    {
+        eq = true;
+        for (int i = 0; i < deg - 1; i++)
+        {
+            if (knots[i] != knots[i+1])
+            {
+                eq = false;
+            }
+        }
+        if (eq)
+        {
+            N = 1.0;
+            return N;
+        }
+    }
+
+    if (x == knots[knots.size()-1])
+    {
+        eq = true;
+        for (int i = knots.size() - deg + 1; i < knots.size() - 1; i++)
+        {
+            if (knots[i] != knots[i+1])
+            {
+                eq = false;
+            }
+        }
+        if (eq)
+        {
+            N = 1.0;
+            return N;
+        }
+    }
+
+    N = basisRecur(x, knots, deg, knotspan);
+    
+    return N;
+}
---
+++ src/tspline.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include "C:\Users\argir\OneDrive\Desktop\tspline.h"
+
+tspline::tspline() {}
+
+tspline::tspline(int _len, int _deg)
+{
+    deg = _deg;
+    len = _len;
+    kVertices = new kVertex [_len];
+}
+
+void tspline::updateKnotVecs()
+{
+    for (int i = 0; i < len; i++)
+    {
+        kVertices[i].updateKnotVec(deg);
+    }
+}
+
+std::vector<double> tspline::evaluate(double s, double t)
+{
+    std::vector<double> sumPB{0.0,0.0,0.0};
+    double sumB = 0.0;
+    for (int i = 0; i < len; i++)
+    {
+        if ((s >= kVertices[i].knotVec.first[0]) && (s <= kVertices[i].knotVec.first[kVertices[i].knotVec.first.size()-1]) && (t >= kVertices[i].knotVec.second[0]) && (t <= kVertices[i].knotVec.second[kVertices[i].knotVec.second.size()-1]))
+        {
+            int knotspanS = 1;
+            int knotspanT = 1;
+
+            double bs = 0.0;
+            double bt = 0.0;
+
+            bs = basisFunTSpl(s, kVertices[i].knotVec.first, deg, knotspanS);
+            bt = basisFunTSpl(t, kVertices[i].knotVec.second, deg, knotspanT);
+            
+            double B = bs * bt;
+            sumPB[0] = sumPB[0] + kVertices[i].cPoint[0]*B;
+            sumPB[1] = sumPB[1] + kVertices[i].cPoint[1]*B;
+            sumPB[2] = sumPB[2] + kVertices[i].cPoint[2]*B;
+            sumB = sumB + B;
+        }
+    }
+    std::vector<double> P{0.0,0.0,0.0};
+    if (sumB < 1e-7)
+    {
+        P = sumPB;
+    }
+    else
+    {
+        P[0] = sumPB[0] / sumB;
+        P[1] = sumPB[1] / sumB;
+        P[2] = sumPB[2] / sumB;
+    }
+
+    return P;
+}
